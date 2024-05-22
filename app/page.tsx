ed16627e120261185b53967e5609e92d6ccca08d
Alex Arasawa <aarasawa@gmail.com>
2024-05-22 00:14:07 -0700

    sorting components

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,10 @@ import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
 import dynamic from 'next/dynamic';
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar/Navbar';
 import styles from './page.module.css';
 
-const Map = dynamic(() => import('./components/Map'), {
+const Map = dynamic(() => import('./components/Map/Map'), {
   ssr: false,
 });
 
